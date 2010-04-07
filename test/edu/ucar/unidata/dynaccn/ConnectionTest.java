c40ac3f56753d30a373f892e2c4f969c9694982f
Steven Emmerson <emmerson@ucar.edu>
2010-04-07 17:26:08 -0600

    Added ConnectionTest.java.

---
+++ test/edu/ucar/unidata/dynaccn/ConnectionTest.java
@@ -0,0 +1,37 @@
+package edu.ucar.unidata.dynaccn;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ConnectionTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testConnection() throws IOException {
+        new Server();
+        final Connection connection = new Connection(InetAddress.getLocalHost());
+
+        connection.close();
+    }
+}
