c84f9c550b51df1c7ba9cf9359980d1a4e66c0cf
Steven Emmerson <emmerson@ucar.edu>
2010-06-02 15:26:33 -0600

    Added default package tests.

---
+++ test/IteratorTest.java
@@ -0,0 +1,68 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Copyright 2010 University Corporation for Atmospheric Research.  All rights
+ * reserved.  See file LICENSE in the top-level source directory for licensing
+ * information.
+ */
+
+/**
+ * Test of the For-Each loop
+ * 
+ * @author Steven R. Emmerson
+ */
+class IteratorTest {
+    static abstract class AbstractIterator<T> implements Iterator<T> {
+        private final T next;
+
+        AbstractIterator() {
+            next = getNext();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return next;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        protected abstract T getNext();
+    }
+
+    static class StringList implements Iterable<String> {
+        @Override
+        public Iterator<String> iterator() {
+            return new AbstractIterator<String>() {
+                private final String string[] = { "initialized", null };
+                private final int    i        = 0;
+
+                @Override
+                protected String getNext() {
+                    return string[i];
+                }
+            };
+        }
+    }
+
+    public static void main(final String[] args) {
+        for (final Iterator<String> iter = new StringList().iterator(); iter
+                .hasNext();) {
+            System.out.println(iter.next());
+        }
+        for (final String string : new StringList()) {
+            System.out.println(string);
+        }
+    }
+}
---
+++ test/SocketTest.java
@@ -0,0 +1,98 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class SocketTest {
+    static final AtomicInteger port    = new AtomicInteger();
+    static final CyclicBarrier barrier = new CyclicBarrier(2);
+
+    public static void main(final String[] args) throws IOException,
+            InterruptedException, BrokenBarrierException,
+            ClassNotFoundException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    runServer();
+                }
+                catch (final Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        runClient();
+    }
+
+    static void runServer() throws IOException, ClassNotFoundException,
+            InterruptedException, BrokenBarrierException {
+        final Selector selector = Selector.open();
+        final ServerSocketChannel servSockChan = ServerSocketChannel.open();
+        final ServerSocket servSock = servSockChan.socket();
+
+        servSockChan.configureBlocking(false);
+        servSockChan.register(selector, SelectionKey.OP_ACCEPT);
+        servSock.bind(new InetSocketAddress(0));
+
+        port.set(servSock.getLocalPort());
+        barrier.await();
+
+        for (;;) {
+            if (selector.select() > 0) {
+                final Iterator<SelectionKey> iter = selector.selectedKeys()
+                        .iterator();
+                final SelectionKey key = iter.next();
+                iter.remove();
+
+                if (key.isAcceptable()) {
+                    final SocketChannel sockChan = ((ServerSocketChannel) key
+                            .channel()).accept();
+                    sockChan.finishConnect();
+                    final Socket socket = sockChan.socket();
+
+                    final ObjectInputStream objectInputStream = new ObjectInputStream(
+                            socket.getInputStream());
+                    final ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                            socket.getOutputStream());
+
+                    final Object obj = objectInputStream.readObject();
+                    System.out.println("Server: " + obj);
+                    objectOutputStream.writeObject(obj);
+
+                    break;
+                }
+            }
+        }
+    }
+
+    static void runClient() throws InterruptedException,
+            BrokenBarrierException, UnknownHostException, IOException,
+            ClassNotFoundException {
+        barrier.await();
+
+        final Socket socket = new Socket(InetAddress.getLocalHost(), port.get());
+
+        final ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                socket.getOutputStream());
+        final ObjectInputStream objectInputStream = new ObjectInputStream(
+                socket.getInputStream());
+
+        objectOutputStream.writeObject(new Integer(0));
+        final Object obj = objectInputStream.readObject();
+
+        System.out.println("Client: " + obj);
+    }
+}
